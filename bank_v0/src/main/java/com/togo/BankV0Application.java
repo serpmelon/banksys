97938151b074601ac046db382303a4ea31c7110d
serpmelon <tyn243222791@126.com>
2019-04-05 06:54:03 +0800

    bank system v0 初始化

---
+++ bank_v0/src/main/java/com/togo/BankV0Application.java
@@ -0,0 +1,13 @@
+package com.togo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankV0Application.class, args);
+	}
+
+}
