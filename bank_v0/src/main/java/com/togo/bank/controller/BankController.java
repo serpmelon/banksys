d0f94a28dcf652ff3a32d2600563ced754f0bf03
serpmelon <tyn243222791@126.com>
2019-04-05 09:54:18 +0800

    存钱取钱逻辑完成，下午开始大并发测试，现在去下棋啦~~

--- bank_v0/src/main/java/com/togo/bank/controller/BankController.java
+++ bank_v0/src/main/java/com/togo/bank/controller/BankController.java
@@ -5,20 +5,35 @@ import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.togo.bank.service.BankService;
+import com.togo.common.pojo.ResultBuilder;
 import com.togo.common.pojo.ResultBuilder.Result;
 
-@RestController("/bank")
+@RestController
+@RequestMapping(value = "/bank")
 public class BankController {
 
 	@Autowired
 	private BankService bankService;
-	
-	@GetMapping(value = "/save/{userId}/money")
+
+	@GetMapping(value = "/test")
+	public Result test() {
+
+		return ResultBuilder.build();
+	}
+
+	@GetMapping(value = "/save/{userId}/{money}")
 	public Result saveMoney(@PathVariable int userId, @PathVariable BigDecimal money) {
 
 		return bankService.saveMoney(userId, money);
+	}
+
+	@GetMapping(value = "/get/{userId}/{money}")
+	public Result getMoney(@PathVariable int userId, @PathVariable BigDecimal money) {
+
+		return bankService.getMoney(userId, money);
 	}
 }
--- bank_v0/src/main/java/com/togo/bank/service/BankService.java
+++ bank_v0/src/main/java/com/togo/bank/service/BankService.java
@@ -1,6 +1,7 @@
 package com.togo.bank.service;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import javax.transaction.Transactional;
 
@@ -11,9 +12,11 @@ import com.togo.bank.dao.AccountRecordRepository;
 import com.togo.bank.dao.AccountRepository;
 import com.togo.bank.dao.UserRepository;
 import com.togo.bank.entity.Account;
+import com.togo.bank.entity.AccountRecord;
 import com.togo.bank.entity.User;
 import com.togo.common.pojo.ResultBuilder;
 import com.togo.common.pojo.ResultBuilder.Result;
+import com.togo.common.util.UuidUtil;
 
 @Service
 @Transactional
@@ -37,6 +40,46 @@ public class BankService {
 				account.setMoney(account.getMoney().add(money));
 				accountRepo.save(account);
 				result = ResultBuilder.build(true, nowMoney + " -> " + account.getMoney());
+				AccountRecord ar = new AccountRecord();
+				ar.setId(UuidUtil.uuidWithoutLine());
+				ar.setCreateTime(new Date());
+				ar.setUserId(userId);
+				ar.setMoney(money);
+				aRecordRepo.save(ar);
+			} else
+				result = ResultBuilder.build(false, "user is invalid");
+		} catch (Exception e) {
+
+			e.printStackTrace();
+			result = ResultBuilder.buildByException(e);
+		}
+
+		return result;
+	}
+
+	public Result getMoney(int userId, BigDecimal money) {
+		Result result = null;
+		try {
+			if (isValidUser(userId)) {
+
+				Account account = accountRepo.findByUserId(userId);
+				BigDecimal nowMoney = account.getMoney();
+				if (nowMoney.compareTo(money) < 0) {
+
+					result = ResultBuilder.build(false, "do not have enough money");
+				} else {
+					account.setMoney(nowMoney.subtract(money));
+					accountRepo.save(account);
+					result = ResultBuilder.build(true, nowMoney + " -> " + account.getMoney());
+
+					AccountRecord ar = new AccountRecord();
+					ar.setId(UuidUtil.uuidWithoutLine());
+					ar.setCreateTime(new Date());
+					ar.setUserId(userId);
+					ar.setMoney(money.negate());
+					aRecordRepo.save(ar);
+				}
+
 			} else
 				result = ResultBuilder.build(false, "user is invalid");
 		} catch (Exception e) {
